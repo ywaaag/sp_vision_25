b22c5813e641ab01837d5a391dc14cab8d3f35ff
kuroneko-avi <kuroneko@njust.edu.cn>
2026-05-11 13:35:50 +0800

    Revert "修改IPPE通过倾角选解"
    This reverts commit 9b8b786ba4b8a11c42ef40fbb8b18ab06afad18a.

--- tasks/auto_aim/solver.hpp
+++ tasks/auto_aim/solver.hpp
@@ -36,18 +36,8 @@ private:
   Eigen::Matrix3d R_gimbal2world_;
 
   void optimize_yaw(Armor & armor) const;
-  void solve_ippe(Armor & armor, const std::vector<cv::Point3f> & object_points) const;
-  void update_pose(Armor & armor, const cv::Vec3d & rvec, const cv::Vec3d & tvec) const;
 
   double armor_reprojection_error(const Armor & armor, double yaw, const double & inclined) const;
-  double ippe_selection_cost(
-    const Armor & armor, const std::vector<cv::Point3f> & object_points, const cv::Vec3d & rvec,
-    const cv::Vec3d & tvec) const;
-  double pose_reprojection_error(
-    const std::vector<cv::Point3f> & object_points, const std::vector<cv::Point2f> & image_points,
-    const cv::Vec3d & rvec, const cv::Vec3d & tvec) const;
-  double lightbar_tilt_angle(const std::vector<cv::Point2f> & image_points) const;
-  bool is_left_tilt_from_lightbar(double lightbar_tilt, ArmorName name) const;
   double SJTU_cost(
     const std::vector<cv::Point2f> & cv_refs, const std::vector<cv::Point2f> & cv_pts,
     const double & inclined) const;
